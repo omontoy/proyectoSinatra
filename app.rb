e23b000dd5bb3f2a222d8b386ed2fa8b7efb1ea2
Orlando Montoya <omb0320@gmail.com>
2016-10-23 00:24:51 -0500

    Primera version del proyecto Sinatra

---
+++ app.rb
@@ -0,0 +1,55 @@
+
+require "sinatra"
+require "make_todo"
+
+def createTarea
+   Tarea.create(params[:task].upcase)
+end
+
+def deleteTarea
+   Tarea.destroy(params[:idTarea])
+end
+
+def completar
+   Tarea.update(params[:completada])
+end
+
+get "/" do  
+   erb :welcome 
+end
+
+post "/home" do
+   erb :homeApp
+end
+
+post "/newTaskInterface" do   
+   erb :newTask
+end
+
+get "/lista" do
+   @hashTareas = Tarea.all
+   erb :listaTareas
+end
+
+post "/creacionTarea" do
+   createTarea
+   redirect '/lista'
+end
+
+post "/tareaCreada" do
+   redirect '/lista'
+end
+
+get "/borrarTareas" do
+   deleteTarea
+   redirect '/lista'
+end
+
+get "/completarTarea" do
+   completar
+   redirect '/lista'
+end
+
+post "/tareasCompletadas" do
+   erb :completas
+end
